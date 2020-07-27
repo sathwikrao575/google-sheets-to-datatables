fa2f25dc0f009ae1e6580abd5a9e6e75ad23dab6
sathwikrao575 <sathwikrao575@gmail.com>
2020-07-27 21:08:15 +0530

    Update table.js

--- table.js
+++ table.js
@@ -6,6 +6,6 @@
  * To use, simply replace the "tq?key=" value in the
  * URL below with your own unique Google document ID
  * 
- * The Google document's sharing must be set to public
+ * The Google document's must be published for that goto: File > "Publish to web" and click "Publish"
  * 
  */

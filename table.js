b873505e0b2542d96fb36b7df783e82a487f65e1
sathwikrao575 <sathwikrao575@gmail.com>
2020-07-27 21:06:01 +0530

    Update table.js

--- table.js
+++ table.js
@@ -1,1 +1,11 @@
 
+/*!
+ * 
+ * Google Sheets To Datatables 1.0 By SKZ
+ * 
+ * To use, simply replace the "tq?key=" value in the
+ * URL below with your own unique Google document ID
+ * 
+ * The Google document's sharing must be set to public
+ * 
+ */
